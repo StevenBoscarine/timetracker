a6599335a11a7dbbdff3a906f4abeebf170a6955
Steven Boscarine <nospam@boscarine.com>
2010-08-29 01:01:20 -0400

    got WS working

--- finddup/src/main/java/com/boscarine/finddup/FileEntry.java
+++ finddup/src/main/java/com/boscarine/finddup/FileEntry.java
@@ -5,10 +5,13 @@ import com.sleepycat.persist.model.PrimaryKey;
 import com.sleepycat.persist.model.Relationship;
 import com.sleepycat.persist.model.SecondaryKey;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
+@XmlRootElement
 public class FileEntry implements Serializable{
 	private static final long serialVersionUID = 1L;
 
@@ -38,18 +41,22 @@ public class FileEntry implements Serializable{
 		
 	}
 
+	@XmlAttribute
 	public String getFileName() {
 		return fileName;
 	}
 
+	@XmlAttribute
 	public long getLastModified() {
 		return lastModified;
 	}
 
+	@XmlAttribute
 	public long getSize() {
 		return size;
 	}
 
+	@XmlAttribute
 	public String getMd5() {
 		return md5;
 	}
